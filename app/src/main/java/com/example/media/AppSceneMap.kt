16b2f0bfd2aac0e965788c85ef8b6d0ea857887c
stefansdsd-ctrl <stefandesain.sdsd@gmail.com>
2026-09-04 12:15:09 +0200

    Slimmere scenes: groepen, extra app-map, adaptive EQ yoga/taal/sport/café/code

--- app/src/main/java/com/example/media/AppSceneMap.kt
+++ app/src/main/java/com/example/media/AppSceneMap.kt
@@ -21,16 +21,22 @@ object AppSceneMap {
             containsAny(p, "bandcamp") -> "vinyl"
             containsAny(p, "castbox", "podcastaddict", "stitcher", "pocketcasts",
                 "overcast", "castro", "antenna") -> "podcast"
-            containsAny(p, "blinkist", "storytel", "scribd", "audible") -> "audiobook"
+            containsAny(p, "blinkist", "storytel", "scribd", "audible", "libro.fm",
+                "libby", "overdrive") -> "audiobook"
             containsAny(p, "youtube.music", "youtubemusic") -> null
             containsAny(p, "primevideo", "netflix", "disney", "videolan", "jellyfin",
                 "plex", "hbo", "nlziet", "videoland", "android.youtube") -> "tv"
             containsAny(p, "teams", "zoom", "meet", "webex", "slack") -> "meeting"
             containsAny(p, "discord", "ts3", "teamspeak") -> "voicechat"
+            containsAny(p, "whatsapp", "telegram", "signal", "viber", "imessage") -> "call"
             containsAny(p, "nsandroid", "9292", "ovinfo", "ov-app") -> "commute"
             containsAny(p, "nos.nl", "nu.nl", "rtlnieuws", "ad.nl", "telegraaf") -> "news"
             containsAny(p, "kindle", "readera", "moon+reader", "librera") -> "audiobook"
             containsAny(p, "tidal", "deezer", "soundcloud", "pandora") -> null
+            containsAny(p, "notion", "obsidian", "vscode", "termux", "jetbrains") -> "coding"
+            containsAny(p, "uber", "bolt", "free.now", "lyft") -> "car"
+            containsAny(p, "klm", "booking", "expedia", "transavia", "ryanair") -> "plane"
+            containsAny(p, "spotify") -> null
             else -> null
         }
     }
