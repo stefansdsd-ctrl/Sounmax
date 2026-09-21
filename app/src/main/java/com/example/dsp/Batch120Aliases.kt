8bbca260bafec28fdc6a6f18ddaac0fa7bdf1608
stefansdsd-ctrl <stefandesain.sdsd@gmail.com>
2026-09-21 19:20:23 +0200

    Batch 120: zoek-aliassen + favorieten-eerst + BestNow/Nieuw

---
+++ app/src/main/java/com/example/dsp/Batch120Aliases.kt
@@ -0,0 +1,21 @@
+package com.example.dsp
+
+object Batch120Aliases {
+    val MAP = mapOf(
+        "herfst" to setOf("herfstwandel"),
+        "herfstwandel" to setOf("herfstwandel"),
+        "ovstoring" to setOf("ovchipstoring"),
+        "praktijkles" to setOf("praktijkles"),
+        "praktijk" to setOf("praktijkles", "praktijklokaal"),
+        "spa" to setOf("spabeurt", "sauna"),
+        "spabeurt" to setOf("spabeurt"),
+        "icstilte" to setOf("icstilteplus"),
+        "stilteplus" to setOf("icstilteplus"),
+        "wfhkind" to setOf("wfhkind"),
+        "thuiswerken" to setOf("wfhkind", "wfh"),
+        "mall" to setOf("mallzondag"),
+        "winkelcentrum" to setOf("mallzondag"),
+        "collegeavond" to setOf("collegeavond"),
+        "avondcollege" to setOf("collegeavond")
+    )
+}
